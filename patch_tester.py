101b386d65bddd0a3c5b45a692109bdc514dddcc
UEFI-code <zhuoshizhang@hotmail.com>
2025-11-25 14:20:10 +0900

    Make patch tester more obviously

--- patch_tester.py
+++ patch_tester.py
@@ -6,7 +6,7 @@ html = requests.get("http://127.0.0.1:5000/").text
 print("Original HTML length:", len(html))
 
 # modify HTML
-html += "<br>Patched by patch_tester.py</br>"
+html = "<br>Patched by patch_tester.py</br>" + html
 
 # submit patched HTML
 html_base64 = base64.b64encode(html.encode("utf-8")).decode("utf-8")
